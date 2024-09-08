2f9e00635084aa48487dde4616475fc5f3d0b54e
Jingluo <380384484@qq.com>
2024-09-08 14:50:06 +0800

    生成报告

--- project01/cypress.config.js
+++ project01/cypress.config.js
@@ -1,8 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   e2e: {
   // 配置文件, 配置Cypress,比如增加默认URL,这将自动将cy.visit()和cy.request()命令与此baseUrl相联系。
     baseUrl : 'http://mobile.qa.roomis.com.cn'
-  },
+  }
+  
+
 });
+
+
